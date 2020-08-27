c10ebc3e71a1f9d76b4c8e0ca60d552722432946
Quinn McCarthy <qmccarth@usc.edu>
2020-08-26 18:48:47 -0700

    Add files via upload

---
+++ Problem3.java
@@ -0,0 +1,17 @@
+package Problem3;
+
+
+public class Problem3 {
+	public static void main(String[]args) {
+	long num = 600851475143L;
+	long i;
+	for(i = 2; i<+num;i++ ) {
+		if(num%i==0) {
+			num/=i;
+			i--;
+		}
+	
+	}
+	System.out.print(i);	
+}
+}
