e6680af82decc432b21a1724e048ab5af1b257f9
Quinn McCarthy <qmccarth@usc.edu>
2020-09-01 15:57:16 -0700

    Update Problem2.java

--- Problem2.java
+++ Problem2.java
@@ -1,5 +1,7 @@
 package Problem2;
-
+/* By considering the terms in the Fibonacci sequence whose values do not exceed
+ *  four million, find the sum of the even-valued terms
+ */
 public class Problem2 {
 		public static void main(String[]args) {
 	     int prevnum = 1;
