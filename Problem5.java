50e6978a8485eaaf0fe6e7b8ab9cf3aef3b5e008
Quinn McCarthy <qmccarth@usc.edu>
2020-09-01 15:58:25 -0700

    Update Problem5.java

--- Problem5.java
+++ Problem5.java
@@ -1,5 +1,7 @@
 package Problem5;
 
+/* What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20? */
+	
 public class Problem5 {
 	public static void main(String[]args) {
 	int checker = 1;
