d8231c26687fdc40c0e4e08e14792c2fe388e836
Quinn McCarthy <qmccarth@usc.edu>
2020-09-01 15:59:02 -0700

    Update Problem6.java

--- Problem6.java
+++ Problem6.java
@@ -1,5 +1,6 @@
 package Problem6;
 
+/* Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum. */
 public class Problem6 {
 	public static void main(String[]args) {
 		int sum = 0;
